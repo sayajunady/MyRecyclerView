d562fae3b17096b47683d5e2b0e7eb025ed4ff09
KOPI KODE <junadhi@sar.ac.id>
2023-11-17 07:24:59 +0700

    Menampilkan halaman detail dengan Intent.

--- app/src/main/java/com/kopikode/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/kopikode/myrecyclerview/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kopikode.myrecyclerview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
@@ -7,6 +8,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val INTENT_PARCELABLE = "OBJECT_INTENT"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -62,6 +67,9 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
         recyclerView.adapter = SuperheroAdapter(this, superheroList){
+            val intent = Intent(this, DetailSuperheroActivity::class.java)
+            intent.putExtra(INTENT_PARCELABLE, it)
+            startActivity(intent)
 
         }
     }
